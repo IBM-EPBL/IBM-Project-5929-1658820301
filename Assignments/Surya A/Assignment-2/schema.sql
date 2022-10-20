7603efb42cb491ed0b129d396145b6c593aa590e
suryamaverick <114295131+suryamaverick@users.noreply.github.com>
2022-10-20 18:23:05 +0530

    Create schema.sql

---
+++ Assignments/Surya A/Assignment-2/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS user_details;
+CREATE TABLE user_details (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+Footer
+© 2022 GitHub, Inc.
